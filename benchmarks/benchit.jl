f15a5ecfb33d8fba4b86c960ba194497a4d74635
Ricardo Rosa <rmsrosa@gmail.com>
2021-07-17 18:57:23 -0300

    add Winston.jl to benchmark

--- benchmarks/benchit.jl
+++ benchmarks/benchit.jl
@@ -2,24 +2,43 @@
 
 using BenchmarkTools
 
+display("""include(joinpath("..", "src", "CairoPlot.jl")""")
 @time include(joinpath("..", "src", "CairoPlot.jl"))
+display("")
+
+display("using Plots; gr()")
 @time using Plots; gr()
+display("using GR")
 @time using GR
+display("using Winston")
+@time using Winston
+display("using .CairoPlot")
 @time using .CairoPlot
 
-Nx = 640
-Ny = 384
-title = "Perturbation\n of a sine wave\nwhat else?"
+title = "Perturbation\n of a sine wave"
 x = range(0, 10π, length=30)
 y = sin.(x/5) .+ 0.3randn(length(x))
 xticks = 1:5:30
 yticks = -1.5:0.5:1.5
 
+display("")
+display("p = Plots.plot(x, y, title=title)")
 @time p = Plots.plot(x, y, title=title)
-@time display(p)
+display("g = GR.plot(x, y, title=title)")
 @time g = GR.plot(x, y, title=title)
+display("w = Winston.plot(x, y, title=title)")
+@time w = Winston.plot(x, y, title=title)
+display("c = CairoPlot.crplot(x, y; xticks, yticks, title)")
+@time c = CairoPlot.crplot(x, y; xticks, yticks, title)
+
+display("")
+display("display(g)")
 @time display(g)
-@time c = CairoPlot.crplot(x, y; xticks, yticks, Nx, Ny, title, padding=(0.00, 0.02, 0.08, 0.02))
+display("display(p)")
+@time display(p)
+display("display(w)")
+@time display(w)
+display("display(c)")
 @time display(c)
 
 #write_to_png(c,"cairoplotsineexample.png")
